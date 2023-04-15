2ceb715253ece81d9ed561d27ea79a59027b184d
Vitalii Vozniak <vitaliivozniak97@gmail.com>
2023-04-15 20:39:33 +0200

    fixed first task

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //TASK 1
 //====================================================
 
-// Array.prototype.customFilter = function (callback, thisArg) {
-// 	let i
-// 	const length = arr.length
-// 	let res = []
+Array.prototype.customFilter = function (callback, thisArg) {
+	let i
+	const length = this.length
+	let res = []
 
-// 	for (i = 0; i < length; i++) {
-// 		if (callback.call(thisArg, arr[i], i, arr)) {
-// 			res.push(arr[i])
-// 		}
-// 	}
+	for (i = 0; i < length; i++) {
+		if (callback.call(thisArg, this[i], i, this)) {
+			res.push(this[i])
+		}
+	}
 
-// 	return res
-// }
+	return res
+}
 
-// Object.keys(Array.prototype).forEach(method => {
-// 	Object.defineProperty(Array.prototype, method, {
-// 		enumerable: false,
-// 	})
-// })
+Object.keys(Array.prototype).forEach(method => {
+	Object.defineProperty(Array.prototype, method, {
+		enumerable: false,
+	})
+})
 
 //TASK 2
 //====================================================
 
-function createDebounceFunction(callback, timer) {
-	let timeout
-	return function (...args) {
-		const context = this
-		clearTimeout(timeout)
-		timeout = setTimeout(function () { callback.apply(context, args) }, timer)
-	}
-}
+// function createDebounceFunction(callback, timer) {
+// 	let timeout
+// 	return function (...args) {
+// 		const context = this
+// 		clearTimeout(timeout)
+// 		timeout = setTimeout(function () { callback.apply(context, args) }, timer)
+// 	}
+// }
