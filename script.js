b74f2d71c7036227b82fd9ee87d878486fc798a8
Vitalii Vozniak <vitaliivozniak97@gmail.com>
2023-04-16 20:41:54 +0200

    [4.3:16.04]: finished third task

--- script.js
+++ script.js
@@ -13,53 +13,83 @@
 
 //TASK 2
 //====================================================
+// function isNotValid(value) {
+// 	return typeof value != "number" || isNaN(value) || !isFinite(value)
+// }
+
+// class Calculator {
+// 	constructor(firstNumber, secondNumber) {
+// 		if (!firstNumber || !secondNumber || isNotValid(firstNumber) || isNotValid(secondNumber)) {
+// 			throw new Error()
+// 		}
+// 		this.firstNumber = firstNumber
+// 		this.secondNumber = secondNumber
+// 		this.setX = this.setX.bind(this)
+// 		this.setY = this.setY.bind(this)
+// 		this.getSum = this.getSum.bind(this)
+// 		this.getMul = this.getMul.bind(this)
+// 		this.getSub = this.getSub.bind(this)
+// 		this.getDiv = this.getDiv.bind(this)
+// 	}
+
+// 	setX(num) {
+// 		if (!num || isNotValid(num)) {
+// 			throw new Error()
+// 		}
+// 		this.firstNumber = num
+// 	}
+// 	setY(num) {
+// 		if (!num || isNotValid(num)) {
+// 			throw new Error()
+// 		}
+// 		this.secondNumber = num
+// 	}
+// 	getSum() {
+// 		return this.firstNumber + this.secondNumber
+// 	}
+// 	getMul() {
+// 		return this.firstNumber * this.secondNumber
+// 	}
+// 	getSub() {
+// 		return Math.abs(this.firstNumber - this.secondNumber)
+// 	}
+// 	getDiv() {
+// 		if (this.secondNumber === 0) {
+// 			throw new Error()
+// 		}
+// 		return this.firstNumber / this.secondNumber
+// 	}
+// }
+
+//TASK 3
+//====================================================
+
 function isNotValid(value) {
 	return typeof value != "number" || isNaN(value) || !isFinite(value)
 }
 
-class Calculator {
-	constructor(firstNumber, secondNumber) {
-		if (!firstNumber || !secondNumber || isNotValid(firstNumber) || isNotValid(secondNumber)) {
+class RickAndMorty {
+	getCharacter(idCharacter) {
+		if (!idCharacter || isNotValid(idCharacter)) {
 			throw new Error()
 		}
-		this.firstNumber = firstNumber
-		this.secondNumber = secondNumber
-		this.setX = this.setX.bind(this)
-		this.setY = this.setY.bind(this)
-		this.getSum = this.getSum.bind(this)
-		this.getMul = this.getMul.bind(this)
-		this.getSub = this.getSub.bind(this)
-		this.getDiv = this.getDiv.bind(this)
+		return fetch(`https://rickandmortyapi.com/api/character/${idCharacter}`).then(
+			(res) => {
+				if (res.status === 200) {
+					return res.json()
+				}
+				return null
+			}
+		)
 	}
-
-	setX(num) {
-		if (!num || isNotValid(num)) {
+	async getEpisode(idEpisode) {
+		if (!idEpisode || isNotValid(idEpisode)) {
 			throw new Error()
 		}
-		this.firstNumber = num
-	}
-	setY(num) {
-		if (!num || isNotValid(num)) {
-			throw new Error()
+		const res = await fetch(`https://rickandmortyapi.com/api/episode/${idEpisode}`)
+		if (res.status === 200) {
+			return res.json()
 		}
-		this.secondNumber = num
+		return null
 	}
-	getSum() {
-		return this.firstNumber + this.secondNumber
-	}
-	getMul() {
-		return this.firstNumber * this.secondNumber
-	}
-	getSub() {
-		return Math.abs(this.firstNumber - this.secondNumber)
-	}
-	getDiv() {
-		if (this.secondNumber === 0) {
-			throw new Error()
-		}
-		return this.firstNumber / this.secondNumber
-	}
-}
-
-//TASK 3
-//====================================================
+}	
